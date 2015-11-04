26301700238b542ee3e9405720556af22b326b06
Paul Smith <catalystmediastudios@gmail.com>
2015-11-04 13:50:20 -0500

    Add mix

--- mix.exs
+++ mix.exs
@@ -5,8 +5,10 @@ defmodule Authentic.Mixfile do
     [app: :authentic,
      version: "0.0.1",
      elixir: "~> 1.1",
+     description: "Auth for Phoenix",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     package: package,
      deps: deps]
   end
 
@@ -17,15 +19,13 @@ defmodule Authentic.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
+  defp package do
+    [
+      maintainers: ["Paul Smith"],
+      licenses: ["MIT"]
+    ]
+  end
+
   defp deps do
     []
   end
